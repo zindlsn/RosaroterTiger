3c8ee94742e7e4f5393852bf5c7bc84ef8b02004
datMaffin <maffinmaffinmaffinmaffin@gmail.com>
2017-07-08 11:46:58 +0200

    Hello World

--- RosaroterTiger/RosaroterTiger/MainPage.xaml.cs
+++ RosaroterTiger/RosaroterTiger/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace RosaroterTiger
         {
             this.InitializeComponent();
         }
+
+        private void TextBox_TextChanged(System.Object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
