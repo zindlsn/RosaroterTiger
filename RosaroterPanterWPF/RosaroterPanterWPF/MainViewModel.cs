8862c2307a9b724c8d378300bd2d7a6a9c745444
Stefan Zindl <st148777@stud.uni-stuttgart.de>
2017-07-08 16:11:27 +0200

    create BaseViewModel, databinding

---
+++ RosaroterPanterWPF/RosaroterPanterWPF/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RosaroterPanterWPF
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _Title;
+
+        public string Title
+        {
+            get {
+                return _Title;
+            }
+            set
+            {
+                if(_Title != value)
+                {
+                    _Title = value;
+                }
+            }
+        }
+
+
+    }
+}
