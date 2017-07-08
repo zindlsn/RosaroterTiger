a13e838d341436e0052a56782a40239ecb268434
Stefan Zindl <st148777@stud.uni-stuttgart.de>
2017-07-08 15:00:05 +0200

    add Pomodoroservice startTask

---
+++ RosaroterPanterWPF/RosaroterPanterWPF/Services/PomodoroService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RosaroterPanterWPF.Services
+{
+    public class PomodoroService
+    {
+        public void startTask(Task task)
+        {
+
+        }
+    }
+}
