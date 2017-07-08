49df1ff06d2976b37bc9f5871b05b921c1765881
Stefan Zindl <st148777@stud.uni-stuttgart.de>
2017-07-09 00:11:17 +0200

    added comments

--- RosaroterPanterWPF/RosaroterPanterWPF/ViewModels/ReviewViewModel.cs
+++ RosaroterPanterWPF/RosaroterPanterWPF/ViewModels/ReviewViewModel.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace RosaroterTigerWPF.ViewModels
 {
-    class ReviewViewModel : BaseViewModel
+    public class ReviewViewModel : BaseViewModel
     {
+
+
+
     }
 }
