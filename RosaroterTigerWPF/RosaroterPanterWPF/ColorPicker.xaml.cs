0735dd3cba93242a99d2355d5afa91fe41a664e8
datMaffin <maffinmaffinmaffinmaffin@gmail.com>
2017-07-09 13:31:40 +0200

    Color Picker

--- RosaroterTigerWPF/RosaroterPanterWPF/ColorPicker.xaml.cs
+++ RosaroterTigerWPF/RosaroterPanterWPF/ColorPicker.xaml.cs
@@ -27,46 +27,56 @@ namespace RosaroterTigerWPF
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             // red
+            model.PickColor(new Color(0xF0, 0xB7, 0xB7));
+            Close();
         }
 
         private void PastBrown_Click(object sender, RoutedEventArgs e)
         {
-
+            model.PickColor(new Color(0xF0, 0xD3, 0xB7));
+            Close();
         }
 
         private void PastYellow_Click(object sender, RoutedEventArgs e)
         {
-
+            model.PickColor(new Color(0xFF, 0xEF, 0xC3));
+            Close();
         }
 
         private void PastGreen_Click(object sender, RoutedEventArgs e)
         {
-
+            model.PickColor(new Color(0xEC, 0xFF, 0xC3));
+            Close();
         }
 
         private void PastBlueGreen_Click(object sender, RoutedEventArgs e)
         {
-
+            model.PickColor(new Color(0xC3, 0xFF, 0xDE));
+            Close();
         }
 
         private void PastMintBlue_Click(object sender, RoutedEventArgs e)
         {
-
+            model.PickColor(new Color(0xC3, 0xFF, 0xFA));
+            Close();
         }
 
         private void PastBlue_Click(object sender, RoutedEventArgs e)
         {
-
+            model.PickColor(new Color(0xC3, 0xE6, 0xFF));
+            Close();
         }
 
         private void PastPurple_Click(object sender, RoutedEventArgs e)
         {
-
+            model.PickColor(new Color(0xCE, 0xC3, 0xFF));
+            Close();
         }
 
         private void PastPink_Click(object sender, RoutedEventArgs e)
         {
-
+            model.PickColor(new Color(0xFA, 0xC3, 0xFF));
+            Close();
         }
     }
 }
